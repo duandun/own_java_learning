42c65bfc2c9e1a1046cf8a6ea43e96b860079401
cr <duandun@gmail.com>
2013-12-05 23:06:19 +0800

    增加了从键盘输入数字

--- src/com/algorithm/qsort/Qsort.java
+++ src/com/algorithm/qsort/Qsort.java
@@ -1,4 +1,10 @@
 package com.algorithm.qsort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * description: øÏÀŸ≈≈–Ú
  * @author jiaqi
@@ -33,11 +39,21 @@ public class Qsort {
 		return left;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		
 		for (int i = 0;i<args.length;i++)
 		System.out.println(args[i]);
-		int[] a = {0,4,3,1,5,6,8,12,45,77,97,5,24,16};
+		
+		
+		Scanner input = new Scanner(System.in);
+		String str = input.nextLine();
+		
+		String[] k = str.split(",");
+		int[] a = new int[k.length];
+		for (int i = 0;i<k.length;i++) {
+			a[i] = Integer.parseInt(k[i]);			
+		}
+		
 		quicksort(a, 0, a.length-1);
 		for (int i = 0;i<a.length;i++) {
 			System.out.print(a[i]);
