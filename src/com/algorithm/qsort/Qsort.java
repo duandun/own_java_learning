393934eef9c78144106dfb941ed4410db8be80f5
cr <duandun@gmail.com>
2013-12-05 22:39:18 +0800

    增加了快排和类的克隆

---
+++ src/com/algorithm/qsort/Qsort.java
@@ -0,0 +1,48 @@
+package com.algorithm.qsort;
+/**
+ * description: øÏÀŸ≈≈–Ú
+ * @author jiaqi
+ *
+ */
+public class Qsort {
+	public static void quicksort(int n[], int left, int right) {
+		int dp;
+		if (left < right) {
+			dp = partition(n, left, right);
+			quicksort(n, left, dp - 1);
+			quicksort(n, dp+1, right);
+		}
+	}
+	
+	public static int partition(int n[], int left, int right) {
+		int pivot = n[left];
+		while (left < right) {
+			while (left < right && n[right] >= pivot) {
+				right--;
+			}
+			if (left < right)
+				n[left++] = n[right];
+			while (left <right && n[left] <= pivot)
+				left++;
+			if (left < right) 
+				n[right--] = n[left];
+			
+		}
+		
+		n[left] = pivot;
+		return left;
+	}
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0;i<args.length;i++)
+		System.out.println(args[i]);
+		int[] a = {0,4,3,1,5,6,8,12,45,77,97,5,24,16};
+		quicksort(a, 0, a.length-1);
+		for (int i = 0;i<a.length;i++) {
+			System.out.print(a[i]);
+			System.out.print(",");
+		}
+	}
+	
+}
---
+++ src/com/cr/cloneclass/TestClone.java
@@ -0,0 +1,42 @@
+package com.cr.cloneclass;
+
+class Student implements Cloneable{
+
+	private int number;
+	
+	public int getNumber(){
+		return number;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	@Override
+	public Object clone() {
+		Student stu = null;
+		try {
+			stu = (Student)super.clone();
+		}catch(CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return stu;
+	}
+}
+
+public class TestClone {
+	public static void main(String[] agrs) {
+		
+		Student stu1 = new Student();
+		stu1.setNumber(12345);
+		Student stu2 = (Student)stu1.clone();
+		
+		System.out.println("学生1: " + stu1.getNumber());
+		System.out.println("学生2：" + stu2.getNumber());
+		
+		stu2.setNumber(54321);
+		
+		System.out.println("学生1: " + stu1.getNumber());
+		System.out.println("学生2：" + stu2.getNumber());
+	}
+}
